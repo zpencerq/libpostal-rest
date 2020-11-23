21ed4254b751a9f7930d77cbce6dbd87945a8083
Spencer Ellinor <zpencerq@users.noreply.github.com>
2020-11-23 16:58:37 -0500

    allow configurable address components more easily through api

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 	expand "github.com/openvenues/gopostal/expand"
@@ -13,13 +14,66 @@ import (
 )
 
 type ExpandRequest struct {
-	Query string `json:"query"`
-	Options expand.ExpandOptions `json:"options,omitempty"`
+	Query             string               `json:"query"`
+	AddressComponents []string             `json:"address_components,omitempty"`
+	Options           expand.ExpandOptions `json:"options,omitempty"`
 }
 
 type ParseRequest struct {
-	Query string `json:"query"`
+	Query   string               `json:"query"`
 	Options parser.ParserOptions `json:"options,omitempty"`
+}
+
+func GetAddressComponent(componentName string) (uint16, error) {
+	var err error
+
+	var result uint16 = expand.AddressNone
+	switch strings.ToLower(componentName) {
+	case "none":
+		result = expand.AddressNone
+	case "any":
+		result = expand.AddressAny
+	case "name":
+		result = expand.AddressName
+	case "house_number":
+		result = expand.AddressHouseNumber
+	case "street":
+		result = expand.AddressStreet
+	case "unit":
+		result = expand.AddressUnit
+	case "level":
+		result = expand.AddressLevel
+	case "staircase":
+		result = expand.AddressStaircase
+	case "entrance":
+		result = expand.AddressEntrance
+	case "category":
+		result = expand.AddressCategory
+	case "near":
+		result = expand.AddressNear
+	case "toponym":
+		result = expand.AddressToponym
+	case "postal_code":
+		result = expand.AddressPostalCode
+	case "po_box":
+		result = expand.AddressPoBox
+	case "all":
+		result = expand.AddressAll
+	default:
+		err = fmt.Errorf("Unknown componentName %s", componentName)
+	}
+
+	return result, err
+}
+
+func ConstructAddressComponentsValue(component_names []string) uint16 {
+	var result uint16 = 0
+	for _, component_name := range component_names {
+		component, _ := GetAddressComponent(component_name)
+		result |= component
+	}
+
+	return result
 }
 
 func main() {
@@ -57,6 +111,10 @@ func ExpandHandler(w http.ResponseWriter, r *http.Request) {
 
 	q, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(q, &req)
+
+	if len(req.AddressComponents) > 0 {
+		req.Options.AddressComponents = ConstructAddressComponentsValue(req.AddressComponents)
+	}
 
 	expansions := expand.ExpandAddressOptions(req.Query, req.Options)
 
